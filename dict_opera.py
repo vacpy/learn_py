6d9bc50b32477d0410573de2f5e51bc99917211a
vacpy <38271500+vacpy@users.noreply.github.com>
2018-04-24 12:05:14 +0800

    Add files via upload

---
+++ dict_opera.py
@@ -0,0 +1,14 @@
+# /usr/bin/python
+# _*_coding:UTF-8_*_
+########################   dict_opera        ###################
+tel = {'sape':4139,'guido':4127,'jack':4098}
+
+print(tel['jack'])
+print(tel['sape'])
+print(tel['guido'])
+tel['vac'] =7107
+
+print(tel)
+print(sorted(tel.keys()))
+print(list(tel.keys()))
+
